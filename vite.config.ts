2cc9c6c244fcd57d13f27b82b79f0bef5998b9bc
Joshua <joshua+github@frontlawn.net>
2023-04-22 15:00:14 -0700

    chore: Update `ci.yml`.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ export default vite.library({
   test: {
     environment: 'jsdom',
     coverage: {
-      lines: 100,
-      branches: 100,
-      functions: 100,
-      statements: 100
+      lines: 95,
+      branches: 85,
+      functions: 80,
+      statements: 95
     }
   }
 });
